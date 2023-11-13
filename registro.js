827aca6776bd2301cee7171bfaab8b21f04cb3fd
User-saj <92058854+User-saj@users.noreply.github.com>
2023-11-13 12:50:59 -0600

    Add files via upload

---
+++ registro.js
@@ -0,0 +1,45 @@
+/* JavaScript para registro.html */
+
+/* ------------------------ acciones iniciales --------------------------- */
+// bloquea el boton de guardad
+var btnGrdr = document.getElementById("guardar");
+btnGrdr.disabled = true;
+btnGrdr.style.cursor = "not-allowed";
+
+/* --------------------   funciones -------------------- */
+// ***** funcion: guarda info del formulario al presionar el boton "guardar"(submit)
+function func_guardaLS(evento) {
+  // previene la accion original del boton submit
+  evento.preventDefault();
+
+  // variables locales para usar con localStorage(key, value)
+  let usuario, info_usuario;
+  // numero inicial de usuarios en Local Storage
+  let numeroUsrs = localStorage.length;
+  // carga la informacion del formulario en variables locales
+  let nCompl = document.getElementById("nUsuario").value;
+  let nContr = document.getElementById("nPass").value;
+  // se llena una variable objeto con los valores obtenidos del formulario
+  info_usuario = {
+    nombre: nCompl,
+    contrasena: nContr,
+  };
+  // se genera una key consecutiva cada vez que se presiona el boton submit
+  usuario = `usuario_${numeroUsrs + 1}`;
+  // se almacena en localStorage el objeto con la info del personaje
+  localStorage.setItem(usuario, JSON.stringify(info_usuario));
+  // carga pagina de inicio
+  window.location.href = "terminos.html";
+}
+
+function chkCondi() {
+  //
+  let terminos = document.getElementById("term_cond").checked;
+  if (terminos) {
+    btnGrdr.disabled = false;
+    btnGrdr.style.cursor = "pointer";
+  } else {
+    btnGrdr.disabled = true;
+    btnGrdr.style.cursor = "not-allowed";
+  }
+}
